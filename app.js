34d2a2071f0a8ea3f7f95171998562760535a506
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-10-27 15:30:43 -0700

    added JS functions and constants.Tried adding toggle feature but it's not working.

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 /* Get DOM Elements */
 const answerBtn = document.getElementById('answer');
 const resetBtn = document.getElementById('reset');
+const promptSection = document.getElementById('prompt');
+const fortuneSection = document.getElementById('fortune');
+const responseP = document.getElementById('response');
 
-/* State */
+const responses = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
-/* Events */
+// my functions
+function toggleSections() {
+    promptSection.classList.toggle('hide');
+    fortuneSection.classList.toggle('hide');
+}
 
-/* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+answerBtn.addEventListener('click', () => {
+    toggleSections();
+    const randInt = Math.floor(Math.random() * responses.length);
+    const randomChoice = responses[randInt];
+    responseP.textContent = randomChoice;
+});
